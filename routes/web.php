9ac90243d1872b947089305881a60d2518a0961f
geraldapuntar-student <gerald.apuntar@cvsu.edu.ph>
2026-05-25 16:14:31 +0800

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+
 // Redirect root to login
 Route::get('/', function () {
     return redirect()->route('login');
